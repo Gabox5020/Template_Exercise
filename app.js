0e92378bdda1b6990d1bc8f2bd736ddd6fa5da95
Gabriel <gaboxroll.2@gmail.com>
2021-12-20 15:02:06 -0300

    Cuarto Ejemplo

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const li = document.createElement('li')
 li.textContent = 'primer elemento'
 
 lista.appendChild(li)
+
+const arrayElement = ['primer elemento', 'segundo', 'tercero']
+
+arrayElement.forEach(item => {
+    console.log(item)
+})
+
